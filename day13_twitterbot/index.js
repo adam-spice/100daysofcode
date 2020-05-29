b437f9be911eac4732a517f7066cfd8e1c8caa81
Adam Spice <spiceybrisket@pop-os.localdomain>
2020-05-29 12:58:53 +1200

    Day 13 made a twitter bot

---
+++ day13_twitterbot/index.js
@@ -0,0 +1,25 @@
+const Twit = require('twit');
+require('dotenv').config();
+
+console.log(process.env.APPLICATION_CONSUMER_KEY);
+
+const T = new Twit({
+  consumer_key: process.env.APPLICATION_CONSUMER_KEY,
+  consumer_secret: process.env.APPLICATION_CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+// start stream and track tweets
+const stream = T.stream('statuses/filter', { track: '#100DaysOfCode' });
+
+// use this to log errors from requests
+function responseCallback(err, data, response) {}
+
+// event handler
+stream.on('tweet', (tweet) => {
+  // retweet
+  T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+  // like
+  T.post('favorites/create', { id: tweet.id_str }, responseCallback);
+});
